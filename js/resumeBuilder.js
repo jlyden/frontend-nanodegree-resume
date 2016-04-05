97d22286e5f55e1ec1fbe0e586c796cb7ae1e454
jjl <jlyden1@alumni.nd.edu>
2016-04-05 18:59:49 -0400

    Feat: Add click logging

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -172,17 +172,21 @@ if (bio.skills.length > 0 ) {
   }
 }
 
-for (job in work.jobs) {
-  $("#workExperience").append(HTMLworkStart);
-  if (work.jobs.hasOwnProperty(job)){
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedEmployer + formattedTitle);
-    $(".work-entry:last").append(formattedDates);
-    $(".work-entry:last").append(formattedLocation);
-    $(".work-entry:last").append(formattedDescription);
+function displayWork(){
+  for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    if (work.jobs.hasOwnProperty(job)){
+      var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+      var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+      var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+      var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+      $(".work-entry:last").append(formattedEmployer + formattedTitle);
+      $(".work-entry:last").append(formattedDates);
+      $(".work-entry:last").append(formattedLocation);
+      $(".work-entry:last").append(formattedDescription);
+    }
   }
 }
+
+displayWork();
