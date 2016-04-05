64259a228703120886092d3e67eab958694ae4b7
jjl <jlyden1@alumni.nd.edu>
2016-04-05 07:05:45 -0400

    Feat: Add name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+ $("#main").append(["jennifer lyden"]);
+
+var awesomeThoughts = " I am jenny and I am AWESOME! "
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+
+$("#main").append(funThoughts)
