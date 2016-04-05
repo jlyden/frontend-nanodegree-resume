8baa8d2f96f40b6391ab86f10ec4bce6d54b22a4
jjl <jlyden1@alumni.nd.edu>
2016-04-05 11:08:08 -0400

    Feat: Add bio data to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,34 @@
-var name = "jennifer lyden";
-var role = "developer";
+var bio = {
+  "name": "jennifer lyden",
+  "role": "developer",
+  "contacts": {
+    "mobile": "(908)868-3185",
+    "email": "jlyden1@alumni.nd.edu",
+    "github": "https://github.com/jlyden",
+    "location": "Bronx, NY"
+  },
+  "welcomeMsg": "I want to work for you!",
+  "skills": ["programming - Python, HTML5, CSS, SQL", "teaching - philosophy, music, tech", "music - piano, guitar, percussion, voice"],
+  "bioPic": "https://i.stack.imgur.com/hB170.jpg?s=328&g=1"
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
+$("#header").append(formattedEmail);
+$("#header").append(formattedMobile);
+$("#header").append(formattedGithub);
+$("#header").append(formattedLocation);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart);
+$("#header").append(formattedSkills);
--- js/test-code.js
+++ js/test-code.js
@@ -17,3 +17,33 @@ var nums = [1,2,3,4,5];
 var end = nums.pop();
 nums.push(end+1);
 // console.log(nums);
+
+name = "cAmEROn PittMAN";
+
+function standardizeNameCapitalization(_name) {
+  var nameArray = _name.split(' ');
+  // manipulate first name and add space
+  var firstName = nameArray[0];
+  firstName = firstName[0].toUpperCase() +
+              firstName.slice(1).toLowerCase() + " ";
+
+  // manipulate second name
+  var lastName = nameArray[1].toUpperCase();
+  var finalName = firstName + lastName;
+
+  return finalName;
+}
+
+function standardizeNameCapitalization2(_name) {
+  var nameArray = _name.split(' ');
+  // manipulate first name
+  nameArray[0] = nameArray[0].slice(0,1).toUpperCase() +
+                 nameArray[0].slice(1).toLowerCase();
+
+  // manipulate second name
+  nameArray[1] = nameArray[1].toUpperCase();
+  var finalName = nameArray.join(' ');
+
+  return finalName;
+}
+// console.log(standardizeNameCapitalization2(name));
