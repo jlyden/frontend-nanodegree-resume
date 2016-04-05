a2d0a5b8728216ac9c9a96d7dddd28ee40001a13
jjl <jlyden1@alumni.nd.edu>
2016-04-05 09:41:50 -0400

    Feat: Working with arrays

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,8 @@
-var name = "jennifer lyden"
-var role = "developer"
+var name = "jennifer lyden";
+var role = "developer";
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-
-str1 = "audacity";
-str1 = "U" + str1.slice(2);
-console.log(str1);
-
-// $("#main").append(["jennifer lyden"]);
-
-// var awesomeThoughts = " I am jenny and I am AWESOME! "
-// console.log(awesomeThoughts);
-
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
-
-// $("#main").append(funThoughts)
---
+++ js/test-code.js
@@ -0,0 +1,19 @@
+// Test Code for JavaScript Basics
+
+// $("#main").append(["jennifer lyden"]);
+// var awesomeThoughts = " I am jenny and I am AWESOME! "
+// console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+// $("#main").append(funThoughts)
+
+str1 = "audacity";
+str1 = str1[1].toUpperCase() + str1.slice(2);
+// console.log(str1);
+
+// var skills = ["awesomeness", "programming", "teaching", "Python"];
+// $("#main").append(skills[0]);
+
+var nums = [1,2,3,4,5];
+var end = nums.pop();
+nums.push(end+1);
+// console.log(nums);
